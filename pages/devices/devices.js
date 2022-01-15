337fbb2a0166574fa1f115fbe8a18002c2a92317
lilingbo <990295334@qq.com>
2022-01-15 22:56:19 +0800

    debug

--- pages/devices/devices.js
+++ pages/devices/devices.js
@@ -1,66 +1,40 @@
-// pages/devices/devices.js
+// pages/login/login.js
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        username: '',
+        password: ''
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    loginSubmit: function(e) {
+        wx.request({
+            url: 'https://www.dosmart.top:10001/business/login',
+            data: {
+                username: this.username,
+                password: this.password,
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success: function(res) {
+                console.log(res.data);
+                wx.showToast({
+                    title: '登录成功',
+                    icon: 'success',
+                    duration: 20000,
+                    success: function() {
+                        setTimeout(function() {
+                            wx.switchTab({
+                                url: '../index/index',
+                            })
+                        }, 2000)
+                    }
+                })
+            }
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+    onClickIcon: function(e) {}
 })
